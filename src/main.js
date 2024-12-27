f349b3f6183678fe1459dd12a64e78e513882687
djumad <bantandjumad@gmail.gmail.com>
2024-12-27 18:11:35 +0900

    seccond commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+AOS.init();
 
 const app = mount(App, {
   target: document.getElementById('app'),
